5c122f93caa942fc1a854b6d37ce74e209a16054
Liux <lx0758@qq.com>
2019-01-11 15:56:54 +0800

    完善工具类

--- boxing/src/main/java/com/liux/android/boxing/BoxingUtil.java
+++ boxing/src/main/java/com/liux/android/boxing/BoxingUtil.java
@@ -3,6 +3,7 @@ package com.liux.android.boxing;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.text.TextUtils;
 
 import java.io.File;
 
@@ -30,8 +31,20 @@ public class BoxingUtil {
      * @return
      */
     public static File getCacheTempFile(Context context) {
-        File file = new File(getCacheDir(context), String.valueOf(System.currentTimeMillis()));
-        return file.exists() ? getCacheTempFile(context) : file;
+        return getCacheTempFile(context, null);
+    }
+
+    /**
+     * 获取缓存目录一个临时文件
+     * @param context
+     * @param suffix
+     * @return
+     */
+    public static File getCacheTempFile(Context context, String suffix) {
+        String fileName = String.valueOf(System.currentTimeMillis());
+        if (!TextUtils.isEmpty(suffix)) fileName += ("." + suffix);
+        File file = new File(getCacheDir(context), fileName);
+        return file.exists() ? getCacheTempFile(context, suffix) : file;
     }
 
     /**
