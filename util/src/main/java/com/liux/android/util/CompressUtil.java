7e62d8803ae88c19e7f70599ad7751762e78af0c
Liux <lx0758@qq.com>
2019-08-16 14:10:08 +0800

    更新UTIL库

--- util/src/main/java/com/liux/android/util/CompressUtil.java
+++ util/src/main/java/com/liux/android/util/CompressUtil.java
@@ -1,78 +1,106 @@
 package com.liux.android.util;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
-import android.media.ExifInterface;
+import android.support.media.ExifInterface;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+/**
+ * 仿微信压缩参数的图片压缩工具类
+ */
 public class CompressUtil {
 
-    public static File compressPictures(Context context, File file) {
-        if (file == null) throw new NullPointerException("Compressed file cannot be empty");
-        if (!file.exists() || !file.isFile() || !file.canRead()) throw new IllegalStateException("Compressed files are not operational");
-        if (!isPictures(file)) return file;
-
-        String imgPath = file.getAbsolutePath();
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(imgPath, options);
-        int srcWidth = options.outWidth;
-        int srcHeight = options.outHeight;
-        ExifInterface exifInterface = null;
+    /**
+     * 图片压缩
+     * @param in
+     * @return
+     */
+    public static byte[] compressPictures(byte[] in) {
+        InputStream inputStream = null;
+        ByteArrayOutputStream byteArrayOutputStream = null;
         try {
-            exifInterface = new ExifInterface(imgPath);
-        } catch (IOException ignore) {}
-
-        options = new BitmapFactory.Options();
-        options.inSampleSize = computeSize(srcWidth, srcHeight);
-        Bitmap decodeBitmap = BitmapFactory.decodeFile(imgPath, options);
-        Bitmap rotatingBitmap = rotatingImage(decodeBitmap, exifInterface);
-        if (decodeBitmap != rotatingBitmap) decodeBitmap.recycle();
-
-        File outFile = DiskUtil.getCacheTempFile(context, "jpg");
-        FileOutputStream fileOutputStream = null;
-        try {
-            fileOutputStream = new FileOutputStream(outFile);
-            rotatingBitmap.compress(Bitmap.CompressFormat.JPEG, 70, fileOutputStream);
-            fileOutputStream.flush();
-            rotatingBitmap.recycle();
-            return outFile;
+            inputStream = new ByteArrayInputStream(in);
+            byteArrayOutputStream = new ByteArrayOutputStream();
+            compressPictures(inputStream, byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
         } catch (Exception e) {
-            return file;
+            e.printStackTrace();
         } finally {
-            if (fileOutputStream != null) {
-                try {
-                    fileOutputStream.close();
-                } catch (IOException e) {}
-            }
+            try {
+                if (inputStream != null) inputStream.close();
+                if (byteArrayOutputStream != null) byteArrayOutputStream.close();
+            } catch (Exception ignore) {}
         }
+        return null;
     }
 
     /**
-     * 判断是否是图片
-     * @param file
+     * 图片压缩
+     * @param in
+     * @param out
      * @return
      */
-    private static boolean isPictures(File file) {
-        String[] ss = file.getName().split("\\.");
-        if (ss.length < 2) return false;
-        String suffix = ss[ss.length - 1];
-        switch (suffix) {
-            case "bmp":
-            case "gif":
-            case "jpg":
-            case "jpeg":
-            case "png":
-                return true;
-            default:
-                return false;
+    public static boolean compressPictures(File in, File out) {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = new FileInputStream(in);
+            outputStream = new FileOutputStream(out);
+            return compressPictures(inputStream, outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null) inputStream.close();
+                if (outputStream != null) outputStream.close();
+            } catch (Exception ignore) {}
         }
+        return false;
+    }
+
+    /**
+     * 图片压缩
+     * @param in
+     * @param out
+     * @return
+     */
+    public static boolean compressPictures(InputStream in, OutputStream out) {
+        if (in == null) throw new NullPointerException("Input can not be empty");
+        if (out == null) throw new NullPointerException("Output cannot be empty");
+
+        byte[] inData = DiskUtil.readStream(in);
+        if (inData == null) throw new NullPointerException("Input read failed");
+
+        ExifInterface exifInterface = null;
+        try {
+            exifInterface = new ExifInterface(new ByteArrayInputStream(inData));
+        } catch (IOException ignore) {}
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeByteArray(inData, 0, inData.length, options);
+        int srcWidth = options.outWidth;
+        int srcHeight = options.outHeight;
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = computeSize(srcWidth, srcHeight);
+        options.inPreferredConfig = Bitmap.Config.ARGB_4444;
+        Bitmap decodeBitmap = BitmapFactory.decodeByteArray(inData, 0, inData.length, options);
+        Bitmap rotatingBitmap = rotatingImage(decodeBitmap, exifInterface);
+        if (decodeBitmap != rotatingBitmap) decodeBitmap.recycle();
+
+        rotatingBitmap.compress(Bitmap.CompressFormat.JPEG, 70, out);
+        rotatingBitmap.recycle();
+        return true;
     }
 
     /**
