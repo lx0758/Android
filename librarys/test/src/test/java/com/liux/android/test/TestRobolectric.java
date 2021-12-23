ad36da3ad2e78e102b1c239a04ad3ddd0eb7f3c5
6x <lx0758@qq.com>
2021-12-23 16:17:52 +0800

    feat(test): 新增 Shadow 演示代码

--- librarys/test/src/test/java/com/liux/android/test/TestRobolectric.java
+++ librarys/test/src/test/java/com/liux/android/test/TestRobolectric.java
@@ -3,7 +3,11 @@ package com.liux.android.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.robolectric.annotation.Config;
 
+@Config(shadows = ShadowBusiness.class)
+@PowerMockIgnore({"com.liux.android.test.*"})
 public class TestRobolectric extends RobolectricTest {
 
     @Test
@@ -13,5 +17,11 @@ public class TestRobolectric extends RobolectricTest {
 
         System.out.println(getContext().getPackageName());
         System.out.println(getContext().getApplicationInfo().packageName);
+    }
+
+    @Test
+    public void testShadow() {
+        Business.staticMethod();
+        new Business().method();
     }
 }
